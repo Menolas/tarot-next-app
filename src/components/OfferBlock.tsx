cbdb06ca8849e640f8f510085ef4702a3a2dacf0
Elena Christensen <olenachristensen777@gmail.com>
2024-08-26 10:39:46 +0300

    animation order

--- src/components/OfferBlock.tsx
+++ src/components/OfferBlock.tsx
@@ -1,32 +1,48 @@
+"use client";
+
 import Hand from "../assests/svg/hand.svg";
+import {SmokeAnimation} from "@/components/SmokeAnimation";
+import {useEffect, useState} from "react";
 
 export default function OfferBlock() {
-    return (
-        <section className="offer-block">
-            <h1 className="offer-block__title title title--primary">
-                <span>Discover</span>
-                <span>Your</span>
-                <span>Fate</span>
-            </h1>
-            <div className="offer-block__screen offer-block__screen--moon">
-                <div className="moon"></div>
-            </div>
+    const [isLoaded, setIsLoaded] = useState(false);
 
-            <div className="offer-block__screen offer-block__screen--cards">
-                {/*<div className="clouds"></div>*/}
-                <div className="inner-wrap">
-                    <button className="btn offer-block__btn offer-block__btn--left">
-                        Embrace <br />the Unknown
-                    </button>
-                    <div className="center">
-                        <div className="card"></div>
-                        <div className="hand"><Hand /></div>
-                    </div>
-                    <button className="btn offer-block__btn offer-block__btn--right">
-                        Join the Circle <br /> of the Chosen
-                    </button>
-                </div>
-            </div>
+    useEffect(() => {
+        setIsLoaded(true);
+        console.log("OfferBlock loaded")
+    }, []);
+    return (
+        <section className={`offer-block ${isLoaded ? "loaded" : ""}`}>
+            {!isLoaded
+                ? <div>Loading...</div>
+                : (
+                    <>
+                        <SmokeAnimation/>
+                        <h1 className="offer-block__title title title--primary">
+                            <span>Discover</span>
+                            <span>Your</span>
+                            <span>Fate</span>
+                        </h1>
+                        <div className="offer-block__screen offer-block__screen--moon">
+                            <div className="moon"></div>
+                        </div>
+                        <div className="offer-block__screen offer-block__screen--cards">
+                            <div className="inner-wrap">
+                                <button className="btn offer-block__btn offer-block__btn--left">
+                                    Embrace <br/>the Unknown
+                                </button>
+                                <div className="center">
+                                    <div className="card"></div>
+                                    <div className="hand"><Hand/></div>
+                                </div>
+                                <button className="btn offer-block__btn offer-block__btn--right">
+                                    Join the Circle <br/> of the Chosen
+                                </button>
+                            </div>
+                        </div>
+                    </>
+                )
+            }
         </section>
     );
 };

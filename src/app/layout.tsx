47ea2e114c6d7b23fbb795161bd072d2a3f41306
Elena Christensen <olenachristensen777@gmail.com>
2024-09-02 00:02:03 +0300

    try to push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "../assets/scss/style.css";
-import Header from "../components/Header";
 import Footer from "@/components/Footer";
 
 const inter = Raleway({ subsets: ["latin"] });
